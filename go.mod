43b74d5457d9aac3e8f605667757522a251aaf5b
AnLe <thanhan.le21@gmail.com>
2021-09-04 14:21:58 +0700

    connect postgres DB

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go_be_github_trending
 
 go 1.15
 
-require github.com/labstack/echo/v4 v4.5.0
+require (
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/labstack/echo/v4 v4.5.0
+	github.com/labstack/gommon v0.3.0
+	github.com/lib/pq v1.10.3
+)
--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
+	"go_be_github_trending/db"
 	"go_be_github_trending/handler"
 )
 
 func main() {
+
+	sql := &db.Sql{
+		Host:     "localhost",
+		Port:     5432,
+		UserName: "postgres",
+		Password: "123456",
+		DbName:   "github",
+	}
+	sql.Connect()
+	defer sql.Close()
+
 	e := echo.New()
 
 	e.GET("/", handler.Welcome)
